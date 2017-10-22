299a16531a69598000733b747c7589fe9fea8d63
Sascha0912 <s.obst.so@live.de>
2017-10-22 22:01:25 +0200

    main method cleaned

--- src/pkgMain/Game.java
+++ src/pkgMain/Game.java
@@ -2,11 +2,12 @@ package pkgMain;
 
 import java.util.Scanner;
 
-public class Game {
-	
-	public static void main(String[] args) {
-		
-		
+public class Game
+{
+
+	public static void main(String[] args)
+	{
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Bitte Zeilen eingeben:");
 		int rows = sc.nextInt();
@@ -14,11 +15,8 @@ public class Game {
 		int columns = sc.nextInt();
 		sc.close();
 		final long timeStart = System.currentTimeMillis();
-		//boolean[][] b = {{true,true,false,false,false},{true,true,false,false,false},{false,false,true,true,false},{false,false,true,true,false},{false,false,false,false,false}};
 		Field f = new Field(rows, columns);
-		
-		//System.out.println(Math.floorMod(0-1, 5));
-		
+
 		f.fill();
 		f.display();
 		f.step();
@@ -29,12 +27,11 @@ public class Game {
 		f.display();
 		f.step();
 		f.display();
-		
+
 		final long timeEnd = System.currentTimeMillis();
-		
+
 		System.out.println("Laufzeit des Algorithmus: " + (timeEnd - timeStart) + " Millisek.");
-		
-		
+
 	}
-	
+
 }
